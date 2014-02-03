205c3b229b140cdb6593ab44a761a5a955291aaf
Dima Markus <dmi3000@gmail.com>
2014-02-03 17:37:37 -0500

    Update links to get dynamic root
    Signed-off-by: Dima Markus <dmi3000@gmail.com>

--- index.php
+++ index.php
@@ -9,10 +9,9 @@
   <div class="row">
     <div class="12-large large-centered columns ">
       <h2>Web Admin Panel</h2>
-      <h3><a href="/webadmin/server_info">Server Info</a></h3>
-      <h3><a href="http://www.dubspot.com/webadmin/banner_admin/buttons.php">Class Closures</a></h3>
-      <h3><a href="http://www.dubspot.com/webadmin/banner_admin/banner_admin.php">Schedule Banner</a></h3>
-      <h3><a href="http://www.dubspot.com/webadmin/banner_admin/upcoming_courses.php">Upcoming Courses</a></h3>
+      <h3><a href="<?php echo $root ?>banner_admin/buttons.php">Class Closures</a></h3>
+      <h3><a href="<?php echo $root ?>banner_admin/banner_admin.php">Schedule Banner</a></h3>
+      <h3><a href="<?php echo $root ?>banner_admin/upcoming_courses.php">Upcoming Courses</a></h3>
     </div>  
   </div>
 
